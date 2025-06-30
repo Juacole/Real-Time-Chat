734d17346cc7bd845fba6a1c512d3d49de72fa3e
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-06-30 05:37:12 -0500

    fix(routing): Se modifica consumers.ChatConsumer a consumers.ChatConsumer.as_asgi()}

--- core/routing.py
+++ core/routing.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
 from . import consumers
 
+'''Establece las conexiones que tendra cada usuario, como se redirigen y como se organizan las conexiones WebSocket.
+
+- Define como y auqe consmidor se va a conectar cada usuario segun la URL
+- Permite que cada conexion websocket llegue al consumidor adecuado, segun la ruta y los parametros, como por ejemplo el nombre de la sala de chat'''
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
