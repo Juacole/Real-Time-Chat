88356853901dfc70fd9ab531c4c073dfc83ca3f3
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-06-30 10:15:01 -0500

    Configuracion basica para tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./chats/templates/**/*.html",
+        "./templates/**/*.html",
+        "./**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
