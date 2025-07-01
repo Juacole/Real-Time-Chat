dbbc5a541f70db2d1987a51d8ef4c97351ee6cfd
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-07-01 13:07:16 -0500

    feat(login): Se añade la logica para recibir peticiones POST en el formulario de login.html

--- login/views.py
+++ login/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .models import User
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/chats/')
+        else:
+            messages.error(request, 'Usuario o contraseña inválidos.')
+            return redirect('/login/')
     return render(request, 'login/login.html')
     
