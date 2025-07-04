0f571904a435172e4c66b15b6c5829416fedfaee
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-07-04 11:17:03 -0500

    feat: Se agregan dos nuevos paths para el mapeo de dos nuevas vistas

--- chats/urls.py
+++ chats/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.chats, name='chats'),
+    path('obtener-contactos/', views.obtener_contactos, name='obtener_contactos'),
+    path('obtener-mensajes/<int:user_id>/', views.obtener_mensajes, name='obtener_mensajes'),
 ]
