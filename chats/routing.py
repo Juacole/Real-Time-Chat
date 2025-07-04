ebabe206cd263b1c7d41b90b9d9aae906c893680
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-07-04 10:43:36 -0500

    refactor(re_path): Se cambia el re_path para definir nuevos puntos de conexion http, como obtener_contactos y obtener_mensjaes

--- chats/routing.py
+++ chats/routing.py
@@ -6,5 +6,8 @@ from . import consumers
 - Define como y auqe consmidor se va a conectar cada usuario segun la URL
 - Permite que cada conexion websocket llegue al consumidor adecuado, segun la ruta y los parametros, como por ejemplo el nombre de la sala de chat'''
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(
+        r'ws/private-chat/(?P<user_id>\d+)/$', 
+        consumers.PrivateChatConsumer.as_asgi()
+    ),
 ]
